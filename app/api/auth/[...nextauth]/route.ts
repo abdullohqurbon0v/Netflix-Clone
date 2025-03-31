f87f9b80d80f2833892d3e11972ecf9dca91bd0b
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-03-31 14:18:00 +0500

    manage accout ui

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,16 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { Session } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+  interface Session {
+    user?: {
+      username?: string;
+      uid?: string;
+    } & Session["user"];
+  }
+}
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -9,16 +20,19 @@ const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }: any) {
-      session.user.username = session.user?.name
-        ?.split(" ")
-        .join("")
-        .toLowerCase();
-      session.user.uid = token.sub;
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (session.user) {
+        session.user.username = session.user.name
+          ?.split(" ")
+          .join("")
+          .toLowerCase();
+        session.user.uid = token.sub;
+      }
       return session;
     },
   },
   secret: process.env.SECRET_KEY,
 };
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
