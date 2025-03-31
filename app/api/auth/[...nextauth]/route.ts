838728726ee6c076b1030c5b956b4f43779a55d7
abdullohqurbon0v <abdullohqurbonov332@gmail.com>
2025-03-31 14:24:14 +0500

    fix configuration and types to eslint

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,21 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, Session } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
+// Extend the default Session type to include custom properties
+declare module "next-auth" {
+  interface Session {
+    user: {
+      name?: string | null; // Default property
+      email?: string | null; // Default property
+      image?: string | null; // Default property
+      username?: string; // Custom property
+      uid?: string; // Custom property
+    };
+  }
+}
+
+// Define auth options
 const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
